70647bd46401e88386e84016298ba73fa12dc365
klm <kylymaratov@gmail.com>
2024-10-09 18:06:14 +0600

    added README file

---
+++ README.md
@@ -0,0 +1,22 @@
+<img src="https://i.ibb.co/RgfL1Kn/banner.webp" alt="Logo" width="120px" height="70px"/>
+
+# Home Theater
+ 
+
+This is a set of applications for Android TV, desktop, mobile devices for watching movies for free and without ads.
+
+ 
+## Usage 
+
+### Run api server from root directory
+
+Command for install depends
+```
+ npm install
+```
+
+Run server
+```
+ npm run start
+
+```
--- desktopapp/main.py
+++ desktopapp/main.py
@@ -7,14 +7,11 @@ class M3U8PlayerApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical')
         
-        # Укажите ваш m3u8 URL
-        m3u8_url = "https://www087.anzeat.pro/streamhls/7bfe1a8ced27da6dd288c40e1e696fa3/ep.1.1709029346.360.m3u8"
-        
-        # Создаем виджет Video
+        m3u8_url = "https://www087.anzeat.pro/streamhls/7bfe1a8ced27da6dd288c40e1e696fa3/ep.1.1709029346.360.m3u8"    
+    
         self.video = Video(source=m3u8_url)
         layout.add_widget(self.video)
-
-        # Запускаем воспроизведение через секунду после старта
+        
         Clock.schedule_once(self.play_video, 1)
         
         return layout
